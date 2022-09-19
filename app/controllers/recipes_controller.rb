f892f22235726d9967e8f134d0c855d2e78ec365
MuT-anT <hashirazam534@gmail.com>
2022-09-19 20:06:53 +0500

    Added new recipes form

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,4 +2,43 @@ class RecipesController< ApplicationController
     def index
         @recipes=Recipe.all
     end
+    def new
+        @recipe=Recipe.new
+    end
+
+    def create
+        @recipe=Recipe.new(recipe_params)
+        @recipe.chef=Chef.first
+        if @recipe.save
+            flash[:notice]="Recipe was created Successfully"
+            redirect_to recipe_path(@recipe)
+        else
+            render 'new' , status: :unprocessable_entity
+        end
+    end
+
+    def show
+        @recipe=Recipe.find(params[:id])
+    end
+
+    def edit
+        @recipe=Recipe.find(params[:id])
+    end
+
+    def update
+        @recipe=Todo.find(params[:id])
+        if @recipe.update(recipe_params)
+            flash[:notice]="Todo was successfully Updated"
+            redirect_to todo_path(recipe)
+        else
+            render 'edit' , status: :unprocessable_entity
+        end
+    end
+
+private
+
+def recipe_params
+    params.require(:recipe).permit(:name,:description)
+end
+
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,6 @@
 class Recipe< ApplicationRecord
-    validates :name,:description, presence: true
+    validates :name, presence: true
+    validates :description, presence: true, length: {minimum: 20}
     belongs_to :chef
     validates :chef_id, presence: true
 end
