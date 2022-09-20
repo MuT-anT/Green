725ee249c0df587c56c08c1474b1427982277fd7
MuT-anT <hashirazam534@gmail.com>
2022-09-20 18:40:13 +0500

    Implemented chef signup form

---
+++ app/controllers/chefs_controller.rb
@@ -0,0 +1,28 @@
+class ChefsController < ApplicationController
+
+
+def new
+    @chef=Chef.new
+end
+
+def create
+    @chef=Chef.new(chef_params)
+    if @chef.save
+        flash[:success]="Welcome #{@chef.name} to MyRecipes"
+        redirect_to chef_path(@chef)
+    else
+        render 'new' , status: :unprocessable_entity
+    end
+end
+
+def show
+    @chef=Chef.find(params[:id])
+end
+
+private
+
+def chef_params
+    params.require(:chef).permit(:name, :email, :password, :password_confirmation)
+end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
   root "pages#home"
   get 'pages/home', to: 'pages#home'
   resources :recipes
+  get '/signup', to: 'chefs#new'
+  resources :chefs, expect: [:new]
 end
